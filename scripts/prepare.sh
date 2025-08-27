3b13eccc9d07edb268c8161a3b283ee0116ab67a
Allen Guo <guosonglin001@gmail.com>
2025-08-27 11:45:33 +0800

    note

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,6 +1,7 @@
 pixi g i openssh git cmake ninja
 pixi g i cmake ninja
 pixi g i python clang clangxx clang-tools
+# use clang + mold from conda on linux
 pixi g i mold
-# use clang + lld from conda on mac
+# use clang + lld from conda on macos
 pixi g i lld
