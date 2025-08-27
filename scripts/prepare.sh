4c273f8a86f33945c71997828e2c8d8e2e5244d2
Allen Guo <guosonglin001@gmail.com>
2025-08-27 11:43:05 +0800

    lld on mac

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,3 +1,6 @@
 pixi g i openssh git cmake ninja
 pixi g i cmake ninja
-pixi g i python clang clangxx clang-tools mold
+pixi g i python clang clangxx clang-tools
+pixi g i mold
+# use clang + lld from conda on mac
+pixi g i lld
