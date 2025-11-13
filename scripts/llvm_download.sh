9ec4e5fd8adf2360cc7c0082a535159808970589
Allen Guo <guosonglin001@gmail.com>
2025-11-13 22:06:31 +0800

    update hash

--- scripts/llvm_download.sh
+++ scripts/llvm_download.sh
@@ -22,13 +22,14 @@ git sparse-checkout add \
   '!/llvm/lib/Target' '/llvm/lib/Target/*.*' \
   '/llvm/lib/Target/RISCV' '/llvm/lib/Target/ARM' '/llvm/lib/Target/AArch64' \
   '/llvm/lib/Target/AMDGPU' '/llvm/lib/Target/NVPTX' '/llvm/lib/Target/X86' \
-  '/llvm/lib/Target/SPIRV' '/llvm/lib/Target/PowerPC'
+  '/llvm/lib/Target/SPIRV' '/llvm/lib/Target/PowerPC' '/llvm/lib/Target/Xtensa'
 
 git sparse-checkout list
 
 ######
 
-LLVM_SHA="dd14eb8242d756f7ac03d003effab289d8950adf"
+LLVM_SHA="f6ded0be897e2878612dd903f7e8bb85448269e5"
+# LLVM_SHA="dd14eb8242d756f7ac03d003effab289d8950adf"
 # LLVM_SHA="69db050839d1bdc47d750287689c46b5417d235e"
 
 git fetch --depth 1 origin $LLVM_SHA
